6757d42986a129e7bd250e9c37adaa4e36ea6d71
Aluno <Aluno@LAB-06-15>
2018-04-05 22:44:48 -0300

    Adicionando mais actions listeners

--- LojaDeInformatica/src/main/java/br/edu/etec/lojainformatica/TelaCadClientes.java
+++ LojaDeInformatica/src/main/java/br/edu/etec/lojainformatica/TelaCadClientes.java
@@ -1,6 +1,11 @@
 package br.edu.etec.lojainformatica;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 public class TelaCadClientes extends TelaDeCadastro{
@@ -17,7 +22,7 @@ public class TelaCadClientes extends TelaDeCadastro{
 	JTextField txtFone = new JTextField();
 	
 	JLabel lbEmail = new JLabel("Email");
-	JTextField txtEmail = new JtextField();
+	JTextField txtEmail = new JTextField();
 	
 	
 	public TelaCadClientes() {
@@ -34,6 +39,39 @@ public class TelaCadClientes extends TelaDeCadastro{
 		this.painelParaCampos.add(lbFone);
 		this.painelParaCampos.add(txtFone);
 		
+		this.painelParaCampos.add(lbEmail);
+		this.painelParaCampos.add(txtEmail);
+		System.out.println("terminando de adicionar os campos, add agora actionlistener...");
+		
+		JScrollPane listScroller = new JScrollPane(list);
+		listScroller.setPreferredSize(new Dimension(250, 80));
+		this.painelListagem.add(list);
+		
+		this.btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TelaCadClientes.this.limparFormulario();
+			}			
+		});
+		
+		this.btnSalvar.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				try {
+						TelaCadClientes.this.salvar();
+				}catch (Exception e1){
+						e1.printStackTrace();
+				}
+			}
+			
+		});
+		
+		this.btnCancelar.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				TelaCadClientes.this.cancelar();
+				
+			}
+		});
 	}
 
 	
